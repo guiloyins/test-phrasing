424b4b77ad5cd1e4c535d6269357c02c6976880f
Rafael Fiuza <rafael.fiuza@helabs.com.br>
2014-03-28 14:08:35 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PPhrasing::Application.config.secret_key_base = '0b98dd5bafbb0c6f7ca87f4aaf5a783289df8bd1759738c51da676ce560cbfdf6d188d3986230c0d4a2e7c35df02608e81ea1c75150dcecdbf1e9ca87ec9ade2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PPhrasing::Application.config.session_store :cookie_store, key: '_p_phrasing_session'
