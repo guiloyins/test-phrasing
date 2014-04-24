79b25e89861056954ebf4b9325736a4aad18c531
Rafael Fiuza <guiloyins@gmail.com>
2014-04-24 12:48:57 -0300

    added strange plugin in plsql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140328172413) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "phrasing_phrase_versions", force: true do |t|
     t.integer  "phrasing_phrase_id"
     t.text     "value"
